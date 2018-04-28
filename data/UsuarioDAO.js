1eb2b155af63243024b588cfafdb2b5c9234f46e
Claudio Acioli <kllaudyo@gmail.com>
2018-04-28 18:58:58 -0300

    feat: Adiciona rotas para usuarios

--- data/UsuarioDAO.js
+++ data/UsuarioDAO.js
@@ -10,4 +10,18 @@ UsuarioDAO.prototype.getUsuario = function (id_usaurio, cb) {
     this._conn.query("call get_usuario(?);", id_usaurio, cb);
 };
 
+UsuarioDAO.prototype.insert = function({id_empresa, nm_usuario, nm_email, nm_senha}, cb){
+    this._conn.query("SET @output = 10");
+    this._conn.query("call post_usuario(@output, ?, ?, ?, ?)", [id_empresa, nm_usuario, nm_email, nm_senha]);
+    this._conn.query("select @output as id_usuario", cb);
+};
+
+UsuarioDAO.prototype.update = function({id_usuario, nm_usuario, nm_email, nm_senha}, cb){
+    this._conn.query("call put_usuario(?,?,?,?)",[id_usuario, nm_usuario, nm_email, nm_senha], cb);
+};
+
+UsuarioDAO.prototype.delete = function(id_usuario, cb){
+    this._conn.query("call delete_usuario(?);", id_usuario, cb);
+};
+
 module.exports = () => UsuarioDAO;
--- routes/usuario.js
+++ routes/usuario.js
@@ -37,4 +37,87 @@ module.exports = app => {
 
     });
 
+    app.post('/usuarios', (req, res) => {
+
+        const usuario = req.body;
+        const conn = app.data.connectionFactory();
+        const dao = new app.data.UsuarioDAO(conn);
+
+        dao.insert(usuario, (err, rs) => {
+
+            if(err){
+                res.status(400).json({err});
+                return;
+            }
+
+            usuario["id_usuario"] = rs[0].id_usuario;
+
+            const result = {
+                status:"1",
+                message:"Incluído com sucesso",
+                payload: usuario
+            };
+
+            res
+                .status(201)
+                .json(result);
+
+        });
+
+    });
+
+    app.put('/usuarios/:id', (req, res) => {
+
+        const id = req.params.id;
+        const usuario = req.body;
+        usuario.id_usuario = id;
+        const conn = app.data.connectionFactory();
+        const dao = new app.data.UsuarioDAO(conn);
+
+        dao.update(usuario, (err, rs) => {
+
+            if(err){
+                res.status(400).json({err});
+                return;
+            }
+
+            const result = {
+                status:"1",
+                message:"Atualizado com sucesso",
+                payload: usuario
+            };
+
+            res
+                .status(200)
+                .json(result);
+        })
+
+    });
+
+    app.delete('/usuarios/:id', (req, res) => {
+
+        const id = req.params.id;
+        const conn = app.data.connectionFactory();
+        const dao = new app.data.UsuarioDAO(conn);
+
+        dao.delete(id, (err, rs) => {
+
+            if(err){
+                res.status(400).json({err});
+                return;
+            }
+
+            const result = {
+                status:"1",
+                message:"Excluído com sucesso"
+            };
+
+            res
+                .status(204)
+                .json(result);
+
+        });
+
+    });
+
 };
