fce20492375851e48eb870a53b7cefcbb33cbcd4
Claudio Acioli <kllaudyo@gmail.com>
2018-04-28 18:42:50 -0300

    feat: Cria outras rotas para categoria

--- data/CategoriaDAO.js
+++ data/CategoriaDAO.js
@@ -10,4 +10,18 @@ CategoriaDAO.prototype.getCategoria = function(id_categoria, cb){
     this._conn.query("call get_categoria(?);", id_categoria, cb);
 };
 
+CategoriaDAO.prototype.insert = function({id_empresa, ds_categoria, tp_categoria}, cb){
+    this._conn.query("SET @output = 10");
+    this._conn.query("call post_categoria(@output, ?, ?, ?)", [id_empresa, ds_categoria, tp_categoria]);
+    this._conn.query("select @output as id_categoria", cb);
+};
+
+CategoriaDAO.prototype.update = function({id_categoria, ds_categoria, tp_categoria}, cb){
+    this._conn.query("call put_categoria(?,?,?)",[id_categoria, ds_categoria, tp_categoria], cb);
+};
+
+CategoriaDAO.prototype.delete = function(id_categoria, cb){
+    this._conn.query("call delete_categoria(?)", id_categoria, cb);
+};
+
 module.exports = () => CategoriaDAO;
--- routes/categoria.js
+++ routes/categoria.js
@@ -37,4 +37,87 @@ module.exports = app => {
 
     });
 
+    app.post('/categorias', (req, res) => {
+
+        const categoria = req.body;
+        const conn = app.data.connectionFactory();
+        const dao = new app.data.CategoriaDAO(conn);
+
+        dao.insert(categoria, (err, rs) => {
+
+            if(err){
+                res.status(400).json({err});
+                return;
+            }
+
+            categoria["id_categoria"] = rs[0].id_categoria;
+
+            const result = {
+                status:"1",
+                message:"Incluído com sucesso",
+                payload: categoria
+            };
+
+            res
+                .status(201)
+                .json(result);
+
+        });
+
+    });
+
+    app.put('/categorias/:id', (req, res) => {
+
+        const id = req.params.id;
+        const categoria = req.body;
+            categoria.id_categoria = id;
+        const conn = app.data.connectionFactory();
+        const dao = new app.data.CategoriaDAO(conn);
+
+        dao.update(categoria, (err, rs) => {
+
+            if(err){
+                res.status(400).json({err});
+                return;
+            }
+
+            const result = {
+                status:"1",
+                message:"Atualizado com sucesso",
+                payload: categoria
+            };
+
+            res
+                .status(200)
+                .json(result);
+        });
+
+    });
+
+    app.delete('/categorias/:id', (req, res) => {
+
+        const id = req.params.id;
+        const conn = app.data.connectionFactory();
+        const dao = new app.data.CategoriaDAO(conn);
+
+        dao.delete(id, (err, rs) => {
+
+            if(err){
+                res.status(400).json({err});
+                return;
+            }
+
+            const result = {
+                status:"1",
+                message:"Excluído com sucesso"
+            };
+
+            res
+                .status(204)
+                .json(result);
+
+        });
+
+    });
+
 };
