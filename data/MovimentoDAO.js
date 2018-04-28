ae826df93bdbb9b8dff6aa10499bf4b59ea1975c
Claudio Acioli <kllaudyo@gmail.com>
2018-04-28 19:26:55 -0300

    feat: Adiciona rotas para  movimento

--- data/MovimentoDAO.js
+++ data/MovimentoDAO.js
@@ -10,4 +10,18 @@ MovimentoDAO.prototype.getMovimento = function(id_movimento, cb){
     this._conn.query("call get_movimento(?);", id_movimento, cb);
 };
 
+MovimentoDAO.prototype.insert = function({id_conta, id_categoria, ds_movimento, dt_previsao, dt_confirmacao, vl_previsto, vl_confirmado}, cb){
+    this._conn.query("SET @output = 10");
+    this._conn.query("call post_movimento(@output, ?, ?, ?, str_to_date(?,'%d/%m/%Y'), str_to_date(?,'%d/%m/%Y'), cast(? as decimal(10,2)), cast(? as decimal(10,2)))", [id_conta, id_categoria, ds_movimento, dt_previsao, dt_confirmacao, vl_previsto, vl_confirmado]);
+    this._conn.query("select @output as id_movimento", cb);
+};
+
+MovimentoDAO.prototype.update = function({id_movimento, id_conta, id_categoria, ds_movimento, dt_previsao, dt_confirmacao, vl_previsto, vl_confirmado}, cb){
+    this._conn.query("call put_movimento(?, ?, ?, ?, str_to_date(?,'%d/%m/%Y'), str_to_date(?,'%d/%m/%Y'), cast(? as decimal(10,2)), cast(? as decimal(10,2)))", [id_movimento, id_conta, id_categoria, ds_movimento, dt_previsao, dt_confirmacao, vl_previsto, vl_confirmado], cb);
+};
+
+MovimentoDAO.prototype.delete = function(id_movimento, cb){
+    this._conn.query("call delete_movimento(?);", id_movimento, cb);
+};
+
 module.exports = () => MovimentoDAO;
--- routes/movimento.js
+++ routes/movimento.js
@@ -24,7 +24,7 @@ module.exports = app => {
         const conn = app.data.connectionFactory();
         const dao = new app.data.MovimentoDAO(conn);
 
-        dao.getMovimentos(id, (err, rs) => {
+        dao.getMovimento(id, (err, rs) => {
 
             if(err){
                 res.status(400).json({err});
@@ -32,6 +32,89 @@ module.exports = app => {
             }
 
             res.status(200).json(rs[0][0]);
+
+        });
+
+    });
+
+    app.post('/movimentos', (req, res) => {
+
+        const movimento = req.body;
+        const conn = app.data.connectionFactory();
+        const dao = new app.data.MovimentoDAO(conn);
+
+        dao.insert(movimento, (err, rs) => {
+
+            if(err){
+                res.status(400).json({err});
+                return;
+            }
+
+            movimento["id_movimento"] = rs[0].id_movimento;
+
+            const result = {
+                status:"1",
+                message:"Incluído com sucesso",
+                payload: movimento
+            };
+
+            res
+                .status(201)
+                .json(result);
+
+        });
+
+    });
+
+    app.put('/movimentos/:id', (req, res) => {
+
+        const id = req.params.id;
+        const movimento = req.body;
+        movimento.id_movimento = id;
+        const conn = app.data.connectionFactory();
+        const dao = new app.data.MovimentoDAO(conn);
+
+        dao.update(movimento, (err, rs) => {
+
+            if(err){
+                res.status(400).json({err});
+                return;
+            }
+
+            const result = {
+                status:"1",
+                message:"Atualizado com sucesso",
+                payload: movimento
+            };
+
+            res
+                .status(200)
+                .json(result);
+        });
+
+    });
+
+    app.delete('/movimentos/:id', (req, res) => {
+
+        const id = req.params.id;
+        const conn = app.data.connectionFactory();
+        const dao = new app.data.MovimentoDAO(conn);
+
+        dao.delete(id, (err, rs) => {
+
+            if(err){
+                res.status(400).json({err});
+                return;
+            }
+
+            const result = {
+                status:"1",
+                message:"Excluído com sucesso"
+            };
+
+            res
+                .status(204)
+                .json(result);
 
         });
 
