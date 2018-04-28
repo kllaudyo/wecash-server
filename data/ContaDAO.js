17029c3ae6ea6d7d8eb71c684e309a9feb6eb267
Claudio Acioli <kllaudyo@gmail.com>
2018-04-28 12:51:32 -0300

    refactor: Refatora retorno de contas

--- data/ContaDAO.js
+++ data/ContaDAO.js
@@ -6,4 +6,8 @@ ContaDAO.prototype.getContas = function(id_empresa, cb){
     this._conn.query("call get_contas(?);", id_empresa, cb);
 };
 
-module.exports = function(){return ContaDAO};
+ContaDAO.prototype.getConta = function(id_conta, cb){
+    this._conn.query("call get_conta(?);", id_conta, cb);
+};
+
+module.exports = () => ContaDAO;
--- routes/conta.js
+++ routes/conta.js
@@ -1,4 +1,5 @@
 module.exports = app => {
+
     app.get('/contas', (req, res) => {
 
         const conn = app.data.connectionFactory();
@@ -11,8 +12,28 @@ module.exports = app => {
                 return;
             }
 
-            res.status(200).json(rs);
+            res.status(200).json(rs[0]);
 
         });
+
+    });
+
+    app.get('/contas/:id', (req, res) => {
+
+        const id = req.params.id;
+        const conn = app.data.connectionFactory();
+        const dao = new app.data.ContaDAO(conn);
+
+        dao.getConta(id, (err, rs) => {
+
+            if(err){
+                res.status(400).json({err});
+                return;
+            }
+
+            res.status(200).json(rs[0][0]);
+
+        });
+
     });
 };
